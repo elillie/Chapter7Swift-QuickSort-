3a473898e82d9863670c7c30463b82ad86a7f1ce
Ethan Lillie <lillieethan@gmail.com>
2014-09-26 15:39:57 -0700

    Initial Commit

---
+++ Chapter7Swift(QuickSort)/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chapter7Swift(QuickSort)
+//
+//  Created by Ethan Lillie on 9/26/14.
+//  Copyright (c) 2014 Algorithms. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
